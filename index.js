65a4917b962852c790920453bde0d196de08bc8d
Fredric McWilliams <fredmcwilliams92@gmail.com>
2021-05-22 19:28:47 -0500

    adding in requiered const

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+// const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of your project? (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+            return true;
+            } else {
+            console.log('Please enter your name!');
+            return false;
+            }
+        }
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
