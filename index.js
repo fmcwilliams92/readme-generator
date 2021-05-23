2df18eab0133ad703ebf56a6de7a83291c29b751
Fredric McWilliams <fredmcwilliams92@gmail.com>
2021-05-22 21:35:55 -0500

    adding in questions and create file function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
-const fs = require('fs');
 const inquirer = require('inquirer');
-// const generateMarkdown = require('./utils/generateMarkdown.js');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -9,19 +9,29 @@ const questions = [
         type: 'input',
         name: 'name',
         message: 'What is the name of your project? (Required)',
+        default: 'readme-generator',
         validate: nameInput => {
-            if (nameInput) {
-            return true;
+            if (nameInput < 1 ) {
+            return console.log('Please enter your project name!');
             } else {
-            console.log('Please enter your project name!');
-            return false;
+            return true;
             }
         }
     },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            return console.log(err);
+        } else {
+            console.log('Success! Your README,md file has been generated');
+        }
+    })
+}
+
+const writeToFileAsync = util.promisify(writeToFile);
 
 // TODO: Create a function to initialize app
 function init() {}
