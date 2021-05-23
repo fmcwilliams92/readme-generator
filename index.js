948e11e4e1cfaba3852f5148d73d446477b50245
Fredric McWilliams <fredmcwilliams92@gmail.com>
2021-05-23 18:39:00 -0500

    corrected questions layout and attempting to get questions to actually prompt on the terminal command

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
+inquirer
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: 'input',
-        name: 'title',
-        message: 'What is the title of your project? (Required)',
-        default: 'Project Title',
+        name: 'username',
+        message: 'What is your GitHub userame? (Required)',
+        default: 'Username',
         validate: function(answer) {
             if (answer < 1 ) {
             return console.log('Please enter your project Title!');
@@ -20,9 +21,22 @@ const questions = [
     },
     {
         type: 'input',
+        name: 'title',
+        message: 'What is the title of your project? (Required)',
+        default: 'Title',
+        validate: function(answer) {
+            if (answer < 1 ) {
+            return console.log('Please enter a Title of your project!');
+            } else {
+            return true;
+            }
+        }
+    },
+    {
+        type: 'input',
         name: 'description',
         message: 'What is the description of your project? (Required)',
-        default: 'Project Description',
+        default: 'Description',
         validate: function(answer) {
             if (answer < 1 ) {
             return console.log('Please enter a description of your project!');
@@ -33,34 +47,60 @@ const questions = [
     },
     {
         type: 'input',
-        message: 'If applicable, please provide any installation instructions for your project.',
-        name: 'instructions'
+        name: 'installation',
+        message: 'How did you install your project? (Required)',
+        default: 'Install',
+        validate: function(answer) {
+            if (answer < 1 ) {
+            return console.log('Please enter any installation instructions for your project!');
+            } else {
+            return true;
+            }
+        }
     },
     {
         type: 'input',
-        message: 'If applicable, please provide any instructions and usage information for your project.',
-        name: 'usage'
+        name: 'instructions',
+        message: 'How does an individual use your project? (Required)',
+        default: 'Instructions',
+        validate: function(answer) {
+            if (answer < 1 ) {
+            return console.log('Please explain how to use this project!');
+            } else {
+            return true;
+            }
+        }
     },
     {
         type: 'input',
-        message: 'If applicable, please provide any contribution guidelines for your project.',
-        name: 'contributions'
-    },
-    {
-        type: 'input',
-        message: "If applicable, provide any tests written for your application and provide examples on how to run them.",
-        name: 'tests'
+        name: 'userInput',
+        message: 'How can users better your project? (Required)',
+        default: 'Advise',
+        validate: function(answer) {
+            if (answer < 1 ) {
+            return console.log('Please enter any consctructive criticism for this project!');
+            } else {
+            return true;
+            }
+        }
     },
     {
         type: 'list',
-        message: 'Choose a licence for your project!',
-        choices: [],
-        name: 'licence'
-    }
+        name: 'licencec',
+        message: 'Choose a licence for your project? (Required)',
+        default: 'Licences',
+        choices: ['Apache', 'GNU', 'MIT', 'IBM', 'Boost', 'Eclipse'],
+        validate: function(answer) {
+            if (answer < 1 ) {
+            return console.log('Please choose a licence from the list provided!');
+            } else {
+            return true;
+            }
+        }
+    },
 ];
 
-// This is to create the README file
-// const writeFile = () => 'Title: Project Title, Description: project description';
+// Build a function to create the README file
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, err => {
         if (err) {
@@ -69,12 +109,18 @@ function writeToFile(fileName, data) {
             console.log('Success! Your README.md file has been generated');
         }
     })
-}
+};
 
 const writeToFileAsync = util.promisify(writeToFile);
 
 // TODO: Create a function to initialize app, AKA prompt the questions
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(function(userInput) {
+    console.log(userInput)
+    writeToFile('README.md', generateMarkdown(userInput));
+    });
+};
 
 // Function call to initialize app
 init();
